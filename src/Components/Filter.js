499bc2f4dd90081686c5686c73b8d8daf9f5f190
svats07 <100149130+svats07@users.noreply.github.com>
2025-04-26 17:36:43 +0530

    Final commit for the day of modification of the code.

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,82 +1,94 @@
 import React, { Component } from "react";
 import Select from "react-dropdown-select";
+import data from "./../Constants/Constant.json";
 
 export class Filter extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedCountry: [],
+			selectedCategory: [],
+			selectedLanguage: [],
+		};
+	}
+
 	componentDidMount() {
-		const modal = new window.bootstrap.Modal(
-			document.getElementById("exampleModal")
-		);
+		const modal = new window.bootstrap.Modal(document.getElementById("exampleModal"));
 		modal.show();
+		this.modal = modal;
 	}
 
 	render() {
+		const { selectedCountry, selectedCategory, selectedLanguage } = this.state;
+
 		return (
 			<div
-				className="modal fade"
+				className="modal fade show"
 				id="exampleModal"
 				tabIndex="-1"
 				aria-labelledby="exampleModalLabel"
-				aria-hidden="true"
+				style={{ display: "block" }}
 			>
 				<div className="modal-dialog">
 					<div className="modal-content">
 						<div className="modal-header">
 							<h1 className="modal-title fs-5" id="exampleModalLabel">
-								Choose Country and Category For the News
+								Choose Filters
 							</h1>
 							<button
 								type="button"
 								className="btn-close"
 								data-bs-dismiss="modal"
 								aria-label="Close"
+								onClick={() => this.modal.hide()}
 							></button>
 						</div>
 						<div className="modal-body">
-							<div>
-								<label htmlFor="exampleDataList" className="form-label">
-									Country
-								</label>
+							<div className="mb-3">
+								<label className="form-label">Country</label>
 								<Select
-									// options={data.countries}
+									options={data.countries}
 									labelField="name"
 									valueField="code"
 									placeholder="Select Country"
-									// values={selectedCountry}
-									// onChange={setSelectedCountry}
-									style={{ minWidth: "200px" }}
+									values={selectedCountry}
+									onChange={(values) => this.setState({ selectedCountry: values })}
 								/>
 							</div>
-							<div>
-								<label htmlFor="exampleDataList" className="form-label">
-									Category
-								</label>
+
+							<div className="mb-3">
+								<label className="form-label">Category</label>
 								<Select
-									// options={data.countries}
+									options={data.categories}
 									labelField="name"
 									valueField="code"
-									placeholder="Select Country"
-									// values={selectedCountry}
-									// onChange={setSelectedCountry}
-									style={{ minWidth: "200px" }}
+									placeholder="Select Category"
+									values={selectedCategory}
+									onChange={(values) => this.setState({ selectedCategory: values })}
 								/>
 							</div>
-							<div>
-								<label htmlFor="exampleDataList" className="form-label">
-									Languages
-								</label>
+
+							<div className="mb-3">
+								<label className="form-label">Language</label>
 								<Select
-									// options={data.countries}
+									options={data.languages}
 									labelField="name"
 									valueField="code"
-									placeholder="Select Country"
-									// values={selectedCountry}
-									// onChange={setSelectedCountry}
-									style={{ minWidth: "200px" }}
+									placeholder="Select Language"
+									values={selectedLanguage}
+									onChange={(values) => this.setState({ selectedLanguage: values })}
 								/>
 							</div>
 						</div>
 						<div className="modal-footer">
-							<button type="button" className="btn btn-primary">
+							<button
+								type="button"
+								className="btn btn-primary"
+								onClick={() => {
+									console.log("Selected Filters:", this.state);
+									this.modal.hide();
+								}}
+							>
 								Save changes
 							</button>
 						</div>
